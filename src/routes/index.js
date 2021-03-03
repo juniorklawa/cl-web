5b592047727cfb8dee1b84ef1af9f9e662de5367
Vinicius Pinheiro <vinicius.pinheiro@wisereducacao.com>
2021-03-03 15:03:09 -0300

    fix: delete exact

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
-import Whiteboard from "../pages/Whiteboard";
-import GoogleDocs from "../pages/GoogleDocs";
-import CollabEditor from "../pages/CollabEditor";
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Whiteboard from '../pages/Whiteboard';
+import GoogleDocs from '../pages/GoogleDocs';
+import CollabEditor from '../pages/CollabEditor';
 
 const Routes = () => {
-  return (
-    <>
-      <Switch>
-        <Route exact path="/">
-          <Dashboard />
-        </Route>
-        <Route exact path="/whiteboard">
-          <Whiteboard />
-        </Route>
+	return (
+		<>
+			<Switch>
+				<Route exact path='/'>
+					<Dashboard />
+				</Route>
+				<Route exact path='/whiteboard'>
+					<Whiteboard />
+				</Route>
 
-        <Route exact path="/collabeditor">
-          <CollabEditor />
-        </Route>
+				<Route exact path='/collabeditor'>
+					<CollabEditor />
+				</Route>
 
-        <Route exact path="/google-docs/:level">
-          <GoogleDocs />
-        </Route>
-      </Switch>
-    </>
-  );
+				<Route path='/google-docs/:level'>
+					<GoogleDocs />
+				</Route>
+			</Switch>
+		</>
+	);
 };
 
 export default Routes;
